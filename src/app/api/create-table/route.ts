1d494ed3716c9070fe70d7e39a499438726afabc
Anderson Nascimento <anderson.nascimento@meta.com.br>
2024-01-19 14:47:51 -0300

    chore: add vercel analytics

--- src/app/api/create-table/route.ts
+++ src/app/api/create-table/route.ts
@@ -2,6 +2,11 @@ import { sql } from '@vercel/postgres'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
+  // return new Response(JSON.stringify({ status: 'ok' }), {
+  //   headers: { 'content-type': 'application/json' },
+  // })
+  return NextResponse.json({ message: 'ok' }, { status: 500 })
+
   // try {
   //   const result = await sql`
   //     CREATE TABLE leads (name varchar(255), email varchar(255), phone_number varchar(255), lead varchar(5))
